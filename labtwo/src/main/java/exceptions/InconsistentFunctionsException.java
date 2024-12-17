04282c184e74b2d7b18b518ecbb6dff040c49983
Kalinin <dvport.g@gmail.com>
2024-12-17 19:40:55 +0400

    create users authorization

--- labtwo/src/main/java/exceptions/InconsistentFunctionsException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class InconsistentFunctionsException extends RuntimeException {
-    public InconsistentFunctionsException() {
-    }
-    public InconsistentFunctionsException(String message){
-        throw new RuntimeException(message);
-    }
-}
